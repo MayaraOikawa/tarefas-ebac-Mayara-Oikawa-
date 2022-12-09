d148cb37b35ac7c548f6a4b04a9f886f41fae014
Mayara Vasconcelos Oikawa <102560227+MayaraOikawa@users.noreply.github.com>
2022-12-08 21:16:09 -0300

    Add files via upload
    exercício do modulo 7

---
+++ Modulo 7 - Exercicio/src/Animal.java
@@ -0,0 +1,70 @@
+/**
+ * Classse para registro de animais atendidos a loja
+ * @author May
+ */
+public class Animal {
+
+    private int codigo;
+    private String nome;
+    private String especie;
+    private String raca;
+    private String cor;
+    private String tutor;
+    private String endereco;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
