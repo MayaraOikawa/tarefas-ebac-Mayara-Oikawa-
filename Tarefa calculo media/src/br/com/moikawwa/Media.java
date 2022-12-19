56d622b0775aabdd1d07df3e26ab2171d01e0c29
Mayara Vasconcelos Oikawa <102560227+MayaraOikawa@users.noreply.github.com>
2022-12-19 18:27:36 -0300

    Módulo 8
    exercício do modulo 8

---
+++ Tarefa calculo media/src/br/com/moikawwa/Media.java
@@ -0,0 +1,19 @@
+package br.com.moikawwa;
+
+public class Media {
+    public static void main(String args []){
+        media();
+    }
+    public static void media() {
+        int nota1 = 4;
+        int nota2 = 10;
+        int nota3 = 8;
+        int nota4 = 6;
+
+        int total = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Media: " + total);
+
+    }
+
+}
