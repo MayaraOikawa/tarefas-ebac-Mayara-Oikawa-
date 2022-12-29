549fd0dc3abc6e1c7ca6e3db6f745c6b72c85eb4
Mayara Vasconcelos Oikawa <102560227+MayaraOikawa@users.noreply.github.com>
2022-12-29 15:11:15 -0300

    Tarefa Wropper
    Efetuado exercício

---
+++ modulo 9 - Tarefa Wroppers/src/Exemplo.java
@@ -0,0 +1,9 @@
+public class Exemplo {
+
+    public static void main(String args []) {
+        int codigo = 1999;
+        System.out.println(codigo);
+        Integer codigo1 = codigo;
+        System.out.println(codigo1);
+    }
+}
